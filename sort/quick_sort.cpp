e1d5a2d269922561048cd4a18c468314b05e3771
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-10 00:00:13 +0530

    .

---
+++ sort/quick_sort.cpp
@@ -0,0 +1,20 @@
+/* *>>>>>Anshit_Bhardwaj<<<<<* */
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i, x, n) for(int i = x; i < n; i++)
+#define dbg(x) cout << x << endl
+#define dbg2(x, y) cout << x << ' ' << y << endl
+#define dbg3(x, y, z) cout << x << ' ' << y << ' ' << z << endl
+#define mod 1000000007
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T, n;
+    cin >> T;
+    while(T--){
+        cin >> n;
+        
+    }
+}
