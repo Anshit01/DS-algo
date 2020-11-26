b45009b60b0c35d8665e330b10bd35f7c75599d4
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-11-26 20:41:06 +0530

    added pbds

---
+++ data_structures/pbds.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    ordered_set pbds;
+    for(int& a: arr){
+        cin >> a;
+        pbds.insert(a);
+    }
+    cout << pbds.size() << endl;
+    for(int i = 0; i < n; i++){
+        cout << *pbds.find_by_order(i) << ' ';
+    }
+    cout << endl;
+    for(int i = 0 ; i < n; i++){
+        cout << pbds.order_of_key(arr[i]) << ' ';
+    }
+    cout << *pbds.find(4) << endl;
+    cout << endl;
+}
